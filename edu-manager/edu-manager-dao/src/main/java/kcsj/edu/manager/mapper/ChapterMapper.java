62aed7efa8146a3e650e3d63b40cbcf103d706b0
LittleTao <1419966866@qq.com>
2018-03-10 17:26:27 +0800

    后台管理公用Dao层
    Change-Id: I96ae51bba6af7c4bcdf0eeaf7fc155c1eb261e1c

---
+++ edu-manager/edu-manager-dao/src/main/java/kcsj/edu/manager/mapper/ChapterMapper.java
@@ -0,0 +1,9 @@
+package kcsj.edu.manager.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+import kcsj.edu.manager.pojo.Chapter;
+
+public interface ChapterMapper extends BaseMapper<Chapter> {
+
+}
---
+++ edu-manager/edu-manager-dao/src/main/java/kcsj/edu/manager/mapper/TchDescMapper.java
@@ -0,0 +1,9 @@
+package kcsj.edu.manager.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+import kcsj.edu.manager.pojo.TchDesc;
+
+public interface TchDescMapper extends BaseMapper<TchDesc> {
+
+}
