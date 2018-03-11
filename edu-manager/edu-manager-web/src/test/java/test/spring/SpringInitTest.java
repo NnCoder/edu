f55ecbae7c7a7736a46000ab5e68a518f2988cca
LittleTao <1419966866@qq.com>
2018-03-11 13:31:52 +0800

    架构更新
    Change-Id: I6356bd3e79919fe18134049d43273a45d7d81fa4

--- edu-manager/edu-manager-web/src/test/java/test/spring/SpringInitTest.java
+++ edu-manager/edu-manager-web/src/test/java/test/spring/SpringInitTest.java
@@ -4,11 +4,15 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import kcsj.edu.manager.mapper.UserMapper;
+
 public class SpringInitTest {
 	
 	@Test
 	public void test() {
 		ApplicationContext atx1 = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-dao.xml");
 		ApplicationContext atx2 = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-service.xml");
+		UserMapper userMapper = atx1.getBean(UserMapper.class);
+		
 	}
 }
